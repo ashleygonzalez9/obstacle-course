c8d2b367ba933b405f2c867efbacb97b27138fb2
ashleygonzalez9 <37843561+ashleygonzalez9@users.noreply.github.com>
2019-05-09 14:23:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// if you want to change something on a DIFFERENT element, you have to call it by name (it's "id")
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+	
+// "el" stands for the "element" that was just clicked
+a = document.getElementById("first");
+b = document.getElementById("second");
+c = document.getElementById("third");
+d = document.getElementById("key");
+e = document.getElementById("stand");
+f = document.getElementById("show");
+g = document.getElementById("my_arrow");
+h = document.getElementById("first_dresser");
+i = document.getElementById("your guess");
+j = document.getElementById("click");
+k = document.getElementById("next");
+l = document.getElementById("switch");
+m = document.getElementById("words");
+
+
+ function studentPage(el) { 
+	document.location.href = "https://ashleygonzalez9.github.io/studentwebsitepage/student_websites.html ";
+} 
+
+function skillsPage(el) { 
+	document.location.href = "https://ashleygonzalez9.github.io/essential_skills/Essential_Skills.html  ";
+} 
+ 
+ 
+/*
+	j.src="image/open_door.PNG";
+	*/
